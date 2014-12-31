2536e0c74a4794d933dec167be8dd7201c6a881b
Wang Xiaoqiang - xiwang <xiaoqiang.wang@acxiom.com>
2014-12-31 14:31:16 +0800

    answer to http_server

---
+++ http_server/program.js
@@ -0,0 +1,15 @@
+var http = require('http'),
+    through = require('through');
+
+var server = http.createServer(function(req, res) {
+  if (req.method === 'POST') {
+    req.pipe(through(function(buf) {
+      this.queue(buf.toString().toUpperCase());
+    })).pipe(res);
+  } else {
+    res.end();
+  }
+});
+
+server.listen(process.argv[2]);
+
